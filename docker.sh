3369da9fdb84efcc3d3c8082d2106258173bdc78
caiqueb13 <54421416+caiqueb13@users.noreply.github.com>
2022-12-03 14:10:07 -0300

    Adicionando o Git Cloner

--- docker.sh
+++ docker.sh
@@ -3,9 +3,7 @@
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo apt install git -y
-git config --global user.name "caiqueb13"
-git config --global user.email "caique_tegas@hotmail.com"
-#git clone git@github.com:caiqueb13/mba-cloud-docker.git -y
+git clone https://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@github.com/caiqueb13/mba-cloud-docker.git
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
