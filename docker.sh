7741d822004023b9da5831683301dd6a2138aa34
caiqueb13 <54421416+caiqueb13@users.noreply.github.com>
2022-12-03 17:15:48 -0300

    Mudança no script

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update
+apt-get update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo apt install git -y
 git clone https://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@github.com/caiqueb13/mba-cloud-docker.git
@@ -8,6 +8,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
+docker swarm leave
 docker swarm init
 cd /mba-cloud-docker
 docker stack deploy -c docker-compose.yml projeto
